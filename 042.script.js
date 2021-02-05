9c888cf736f70c5361e63dd8a702bde105262938
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-02-06 01:20:49 +0530

    Dom - Challenge

---
+++ 042.script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const container = document.querySelector("div");
+const image = document.querySelector("img");
+
+container.style.textAlign = "center";
+
+image.style.width = "150px";
+image.style.borderRadius = "50%";
