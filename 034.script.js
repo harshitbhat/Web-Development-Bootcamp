e9576c4984e6f109750c6d8686ff9f9286d2c004
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-02-05 12:45:46 +0530

    For Each Array Method

---
+++ 034.script.js
@@ -0,0 +1,49 @@
+// const numbers = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+//   6,
+//   7,
+//   8,
+//   9,
+//   10,
+//   11,
+//   12,
+//   13,
+//   14,
+//   15,
+//   16,
+//   17,
+//   18,
+//   19,
+//   20,
+// ];
+
+// numbers.forEach(function (el) {
+//   console.log(el * el);
+// });
+
+const movies = [
+  {
+    title: "Amadeus",
+    score: 99,
+  },
+  {
+    title: "Stand By Me",
+    score: 85,
+  },
+  {
+    title: "Parasite",
+    score: 95,
+  },
+  {
+    title: "Alien",
+    score: 90,
+  },
+];
+
+movies.forEach(function (el) {
+  console.log(`${el.title} - ${el.score}/100`);
+});
