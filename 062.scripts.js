399ab475d884eea43944d3999fead0729065967d
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-03-02 14:48:32 +0530

    Added defaultProps

--- 062.scripts.js
+++ 062.scripts.js
@@ -5,14 +5,12 @@ class App extends React.Component {
         <Hello
           to="Abhishek"
           from="Harshit"
-          bangs={3}
           data={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]}
           isFunny={true}
           img="https://images.unsplash.com/photo-1520312622401-cac590a0d473"
         />
         <Hello
           to="Anshu"
-          from="Joy"
           bangs={7}
           data={[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]}
           isFunny={false}
