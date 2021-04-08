36b0a4e0defc5b2ac110e9e3d93345d2e3e45029
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-04-08 15:51:41 +0530

    ES6 Classes

--- 067.script2.js
+++ 067.script2.js
@@ -14,6 +14,14 @@ const Person = function (firstName, birthYear) {
 };
 // Arrow function will not work as it does not have its own this method
 
+// Static Method
+Person.hey = function () {
+  console.log(`Hey There 😃`);
+  console.log(this);
+};
+
+Person.hey();
+
 const jonas = new Person('Jonas', 1991);
 
 // 1. A new empty object is created.
@@ -136,6 +144,76 @@ mercedes.brake();
 mercedes.accelerate();
 mercedes.accelerate();
 
+console.log(' ==================== ES 6 ====================');
 /* -------------------------------------------------------------------------- */
 /*                                ES 6 Classes                                */
 /* -------------------------------------------------------------------------- */
+
+// Class Decleration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  calcAge() {
+    console.log(2021 - this.birthYear);
+  } // Added to Prototype
+
+  // Setters and Getters
+  get age() {
+    return 2021 - this.birthYear;
+  }
+
+  // Static Method
+  static hey() {
+    console.log(`Hey There 😃`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1992);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+PersonCl.prototype.greet = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+
+jessica.greet();
+
+PersonCl.hey();
+
+// Classes are not hoisted. - Must be declared first
+// Classes are also first class citizens. - Pass and return from functions
+// Classes are executed in strict mode.
+
+const account = {
+  owner: 'Jonas',
+  movements: [100, 200, 300, 400],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+
+console.log(account.movements);
+
+console.log(jessica.age);
+
+/* -------------------------------------------------------------------------- */
+/*                                Object.create                               */
+/* -------------------------------------------------------------------------- */
+
+const PersonProto = {
+  calcAge() {
+    console.log(2021 - this.birthYear);
+  },
+};
+
+const steven = Object.create(PersonProto);
